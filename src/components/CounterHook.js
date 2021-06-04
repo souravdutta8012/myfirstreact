682b087aa6beadc93153eeb03bda0c17c14433e9
souravdutta8012 <souravdutta2108@gmail.com>
2021-06-04 22:47:25 +0530

    changes

---
+++ src/components/CounterHook.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+function CounterHook() {
+
+    const [count, setCount] = useState(0)
+
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>Count {count}</button>
+        </div>
+    )
+}
+
+export default CounterHook
