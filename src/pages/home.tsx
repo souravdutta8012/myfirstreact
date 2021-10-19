5832a3828fa9f801fe51bd7c888eb334d31e46b8
souravdutta8012 <MY_NAME@example.comsouravdutta2108@gmail.com>
2021-10-19 17:39:57 +0530

    changes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,19 @@
 import React, { useEffect } from "react";
 import IPage from "../interfaces/page";
 import logging from "../config/logging";
+import NavBar from "./nav";
 
 const HomePage: React.FunctionComponent<IPage> = props => {
     useEffect(() => {
         logging.info(`Loading ${props.name}`);
     }, [props.name])
 
-    return <p>This is the home page!</p>
+    return (
+        <>
+            <NavBar />
+            <p>This is the home page!</p>
+        </>
+    )
 }
 
 export default HomePage;
---
+++ src/pages/nav.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavBar: React.FunctionComponent = props => {
+    return <p>This is the nav bar!</p>
+}
+
+export default NavBar;
