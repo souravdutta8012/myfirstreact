61d306dba5859072a95ae5cd1c45defb2143b510
souravdutta8012 <MY_NAME@example.comsouravdutta2108@gmail.com>
2021-10-19 22:32:05 +0530

    changes

--- src/pages/nav/nav.tsx
+++ src/pages/nav/nav.tsx
@@ -1,18 +1,51 @@
 import React from "react";
 import "./nav.css";
 import { AiOutlineMenu } from 'react-icons/ai'
-import { Col, Row } from "react-bootstrap";
+import { BiSearch } from 'react-icons/bi'
+import { IoMdNotifications } from 'react-icons/io'
+import { FaRegUserCircle } from 'react-icons/fa'
+import { IoIosArrowDown } from 'react-icons/io'
+import { Col, Row, Button, InputGroup, FormControl } from "react-bootstrap";
 
 const NavBar: React.FunctionComponent = props => {
     return (
-        <Row className='body'>
-            <Col className='content'>
-                <AiOutlineMenu size={25} />
-                <span className='app-name'>
-                    IMS
-                </span>
-            </Col>
-        </Row>
+        <>
+            <div className='content'>
+                <Row className='allign'>
+                    <Col className='col-2'>
+                        <AiOutlineMenu size={25} />
+                        <span className='app-name'>
+                            IMS
+                        </span>
+                    </Col>
+                    <Col className='col-2'>
+                        <Button variant="primary" className='live-btn'>
+                            Live
+                        </Button>
+                        <Button variant="outline-light" className='training-btn'>
+                            Training
+                        </Button>
+                    </Col>
+                    <Col className='col-4'>
+                        <InputGroup>
+                            <FormControl className='input-field' placeholder="Search Incident" aria-label="Amount (to the nearest dollar)" />
+                            <InputGroup.Text className='input-field'><BiSearch size={20} /></InputGroup.Text>
+                        </InputGroup>
+                    </Col>
+                    <Col className='col-2 align-right'>
+                        My Worklist
+                    </Col>
+                    <Col className='col-1 align-right'>
+                        <IoMdNotifications size={25} />
+                    </Col>
+                    <Col className='col-1 align-middle'>
+                        <FaRegUserCircle size={25} />
+                        Jhon Doe
+                        <IoIosArrowDown size={20} />
+                    </Col>
+                </Row>
+            </div>
+        </>
     )
 }
 
