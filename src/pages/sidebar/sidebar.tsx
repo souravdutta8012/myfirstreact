564217ac536c4728574eec564d108a01fa531df3
souravdutta8012 <MY_NAME@example.comsouravdutta2108@gmail.com>
2021-10-21 23:25:30 +0530

    changes

--- src/pages/sidebar/sidebar.tsx
+++ src/pages/sidebar/sidebar.tsx
@@ -1,22 +1,24 @@
 import React from "react";
 import "./sidebar.css"
+import { FaTachometerAlt, FaExclamationTriangle, FaFileSignature, FaFileAlt, FaCalendarDay, FaChartLine, FaClipboardCheck, FaClipboardList, FaMapMarkedAlt, FaUsers, FaUserCog } from 'react-icons/fa';
 
 const SideBar: React.FunctionComponent = props => {
 
     return (
         <>
             <div className='sidebar'>
-                <ul>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
-                    <li>1</li>
+                <ul className='list'>
+                    <li><FaTachometerAlt size='20' /></li>
+                    <li><FaExclamationTriangle size='20' /></li>
+                    <li><FaFileSignature size='20' /></li>
+                    <li><FaFileAlt size='20' /></li>
+                    <li><FaCalendarDay size='20' /></li>
+                    <li><FaChartLine size='20' /></li>
+                    <li><FaClipboardCheck size='20' /></li>
+                    <li><FaClipboardList size='20' /></li>
+                    <li><FaMapMarkedAlt size='20' /></li>
+                    <li><FaUsers size='20' /></li>
+                    <li><FaUserCog size='20' /></li>
                 </ul>
             </div>
         </>
