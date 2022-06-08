7ed0a605bdf9ec2b0abbbd22e0d227bc16602bc5
souravdutta8012 <MY_NAME@example.comsouravdutta2108@gmail.com>
2022-06-08 21:23:45 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import HeaderBar from './components/header/HeaderBar'
+import HomePage from './components/home/HomePage';
 
 function App() {
   return (
-    <div className="font-semibold">
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <HeaderBar />
+      <Routes>
+        <Route path='/' element={<Navigate to="/home" />} />
+        <Route path="/home" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/header/HeaderBar.tsx
@@ -0,0 +1,9 @@
+import { AppBar } from "@mui/material"
+
+export default function HeaderBar() {
+    return (
+        <AppBar position="sticky" className="bg-red-400">
+            header bar
+        </AppBar>
+    )
+}
