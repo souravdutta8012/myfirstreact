a2361eae04f0887c789bfbc25d6f36e8c3f37dbb
souravdutta8012 <MY_NAME@example.comsouravdutta2108@gmail.com>
2022-06-08 22:19:03 +0530

    changes

--- src/components/header/HeaderBar.tsx
+++ src/components/header/HeaderBar.tsx
@@ -1,12 +1,34 @@
-import { AppBar } from "@mui/material"
+import { AppBar, Box } from "@mui/material"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faBars } from "@fortawesome/pro-solid-svg-icons"
+import { faCircleUser } from "@fortawesome/pro-duotone-svg-icons"
 
 export default function HeaderBar() {
     return (
-        <AppBar position="sticky" className="bg-red-400">
-            <FontAwesomeIcon icon={faBars} size="lg" />
-            header bar
+        <AppBar position="sticky" className="bg-blue-500 p-2">
+            <Box className="flex items-center justify-between">
+                <Box className="flex items-center">
+                    <Box className="mr-4">
+                        <FontAwesomeIcon icon={faBars} size="lg" />
+                    </Box>
+                    <Box className="text-2xl font-semibold">
+                        To Do App
+                    </Box>
+                </Box>
+                <Box className="flex items-center">
+                    <Box className="mr-4">
+                        <FontAwesomeIcon icon={faCircleUser} size="3x" />
+                    </Box>
+                    <Box className="flex flex-col items-center">
+                        <Box className="text-sm">
+                            Welcome
+                        </Box>
+                        <Box className="text-2xl font-semibold">
+                            Sourav
+                        </Box>
+                    </Box>
+                </Box>
+            </Box>
         </AppBar>
     )
 }
