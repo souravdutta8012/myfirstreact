c57fb5aca0ccfaad1f2bd8016f8dcb404263690e
souravdutta8012 <MY_NAME@example.comsouravdutta2108@gmail.com>
2022-06-08 21:33:15 +0530

    changes

--- src/components/header/HeaderBar.tsx
+++ src/components/header/HeaderBar.tsx
@@ -1,8 +1,11 @@
 import { AppBar } from "@mui/material"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/pro-solid-svg-icons"
 
 export default function HeaderBar() {
     return (
         <AppBar position="sticky" className="bg-red-400">
+            <FontAwesomeIcon icon={faBars} size="lg" />
             header bar
         </AppBar>
     )
